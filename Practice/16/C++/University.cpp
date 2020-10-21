187eb46a1462505c99a0db1d9fbeda5c62d788c5
Rain0Ash <30903358+Rain0Ash@users.noreply.github.com>
2020-10-21 22:16:54 +0300

    c++ 16, 17

---
+++ Practice/16/C++/University.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <regex>
+#include <sstream>
+#include <string>
+
+const std::regex regex = std::regex("^a[a-z][0-9]{2}55661$");
+bool is_match(const std::string& str)
+{
+    return std::regex_match(str, regex);
+}
+
+bool try_get_input(int& x)
+{
+    std::cin >> x;
+    if (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(32767, '\n');
+        return false;
+    }
+
+    std::cin.ignore(32767, '\n');
+    return true;
+}
+
+void try_read(int& value)
+{
+    while (!try_get_input(value))
+    {
+        std::cout << "Неверный ввод. Повторите попытку." << std::endl;
+    }
+}
+
+size_t split(const std::string& str, std::vector<std::string>& strings, const char delimiter = ' ')
+{
+    size_t pos = str.find(delimiter);
+    size_t start = 0;
+    strings.clear();
+
+    // Decompose statement
+    while (pos != std::string::npos) {
+        strings.push_back(str.substr(start, pos - start));
+        start = pos + 1;
+
+        pos = str.find(delimiter, start);
+    }
+
+    // Add the last one
+    strings.push_back(str.substr(start, std::min(pos, str.size()) - start + 1));
+
+    return strings.size();
+}
+
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите число." << std::endl;
+
+    int n;
+
+    do
+    {
+        try_read(n);
+
+        if (n < 1)
+        {
+            std::cout << "Число меньше 1. Повторите ввод." << std::endl;
+        	continue;
+        }
+
+        break;
+
+    } while (true);
+
+    std::vector<std::string> tickets;
+
+    std::string input;
+    std::getline(std::cin, input);
+
+    split(input, tickets);
+
+    std::stringstream stream;
+
+    for (int i = 0; i < tickets.size(); i++)
+    {
+        if (is_match(tickets[i]))
+        {
+            stream << tickets[i];
+
+            if (i < tickets.size() - 1)
+            {
+                stream << " ";
+            }
+        }
+    }
+
+    std::string result = stream.str();
+
+	if (result.empty())
+	{
+        result = "-1";
+	}
+
+    std::cout << result << std::endl;
+}
---
+++ Practice/17/C++/University.cpp
@@ -0,0 +1,127 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <unordered_set>
+
+bool try_get_input(int& x)
+{
+    std::cin >> x;
+    if (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(32767, '\n');
+        return false;
+    }
+
+    std::cin.ignore(32767, '\n');
+    return true;
+}
+
+const int e = 37;
+
+void try_read(int& value)
+{
+    while (!try_get_input(value) || value > e - 1)
+    {
+        std::cout << (value > (e - 1) ? "Значение больше 36. Повторите ввод." : "Неверный ввод. Повторите попытку.") << std::endl;
+    }
+}
+
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    const int k = 12;
+    
+    const bool redblack[e] = 
+    {         false,
+        true, false, true,
+        false, true, false,
+        true, false, true,
+        false, false, true,
+        false, true, false,
+        true, false, true,
+        true, false, true,
+        false, true, false,
+        true, false, true,
+        false, false, true,
+        false, true, false,
+        true, false, true
+    };
+
+    int iteration = 0;
+	
+    int values[e] = {};
+	
+    std::vector<int> buffer(k);
+    std::fill(std::begin(buffer), std::end(buffer), INT32_MIN);
+	
+    int red = 0;
+    int black = 0;
+	
+    int value;
+	
+	do
+	{
+        try_read(value);
+
+		if (value < 0)
+		{
+            break;
+		}
+
+        ++values[value];
+        buffer[iteration % k] = value;
+
+        if (value != 0)
+        {
+            redblack[value] ? ++red : ++black;
+        }
+
+        const int max = *std::max_element(std::begin(values), std::end(values));
+		
+        std::stringstream stream;
+
+        std::string result;
+
+		for (int i = 0; i < e; i++)
+		{
+			if (values[i] == max)
+			{
+                stream << i << " ";
+			}
+		}
+
+        result = stream.str();
+
+        if (!result.empty())
+        {
+            result.resize(result.size() - 1);
+        	stream.str(std::string());
+        }
+
+        std::cout << result << std::endl;
+        
+		for (int i = 0; i < e; i++)
+		{
+			if (!std::any_of(std::begin(buffer), std::end(buffer), [&](const int val) { return val == i; }))
+			{	
+                stream << i << " ";
+			}
+		}
+
+        result = stream.str();
+
+		if (!result.empty())
+		{
+            result.resize(result.size() - 1);
+            stream.str(std::string());
+		}
+        
+        std::cout << result << std::endl;
+
+        std::cout << red << " " << black << std::endl << std::endl;
+		
+    } while (++iteration);
+}
