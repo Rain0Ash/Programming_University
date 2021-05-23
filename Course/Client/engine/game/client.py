88ac5181b8709ffc404affdce1298f031fc9b8cf
Rain0Ash <30903358+Rain0Ash@users.noreply.github.com>
2021-05-24 00:10:26 +0300

    Delete unused code

--- Course/Client/engine/game/client.py
+++ Course/Client/engine/game/client.py
@@ -1,10 +1,8 @@
 from typing import List
 
 import cv2
-import imutils
 
 from engine.game.server import WindowServer, Status
-from logger.logger import logger, LogMessageType
 from helper import helper
 
 
@@ -55,40 +53,3 @@ class WindowClient:
             screen = cv2.resize(screen, (self.scale[0], self.scale[1]), interpolation=cv2.INTER_AREA)
 
         return screen
-
-    def processed_image(self, handler=None):
-        if WindowServer.status == Status.Crashed:
-            return None
-
-        image = self.get_capture()
-
-        if image is None:
-            return None
-
-        if handler is None:
-            return image
-
-        return handler(image)
-
-    def show(self, show, resize=None, handler=None):
-        if WindowServer.status == Status.Crashed:
-            return
-
-        if not self.show_name:
-            logger.log("You need to assign a name first", LogMessageType.Error)
-            return
-
-        if not show:
-            cv2.destroyWindow(self.show_name)
-            return
-
-        image = self.processed_image(handler)
-
-        if image is None:
-            return
-
-        if resize is not None:
-            image = imutils.resize(image, width=resize)
-
-        cv2.imshow(self.show_name, image)
-        cv2.waitKey(25)
